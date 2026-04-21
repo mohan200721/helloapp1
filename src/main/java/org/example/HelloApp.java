09b8e0fd7415280f1e98f7c252dbfdf9683dba34
mohan-kumar <mohankumar2007kommineni@gmail.com>
2026-04-21 15:13:03 +0530

    hello

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,27 +1,14 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        // If no arguments
+        String names;
+
         if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
+            names = "World";
+        } else {
+            names = String.join(", ", args);
         }
 
-        StringBuilder nameBuilder = new StringBuilder();
-
-        // Step 1: Add ALL names with comma
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-        // Step 2: Remove last ", "
-        if (nameBuilder.length() > 0) {
-            nameBuilder = new StringBuilder(
-                    nameBuilder.substring(0, nameBuilder.length() - 2)
-            );
-        }
-
-        // Step 3: Print
-        System.out.println("Hello, " + nameBuilder + "!");
+        System.out.println("Hello, " + names + "!");
     }
 }
